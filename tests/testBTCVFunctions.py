491b9541a21691ec2bbc658406b2285750c5ab74
harzo <harzojunior@gmail.com>
2020-12-08 12:36:25 +0100

    Add BTCV functions tests

---
+++ tests/testBTCVFunctions.py
@@ -0,0 +1,38 @@
+
+from btchip.btchip import btchip
+from btchip.btchipComm import getDongle
+from btchip.btchipUtils import compress_public_key
+
+
+def sha256(text):
+    import hashlib
+    m = hashlib.sha256()
+    m.update(bytearray.fromhex(text))
+
+    return m.digest()
+
+
+dongle = getDongle(True)
+app = btchip(dongle)
+
+# Set passwords
+# empty password
+app.setBTCVPassword(bytearray(1), btchip.BTCV_PASSWORD_INSTANT)
+
+instantPassword = "instantPassword".encode('utf-8')
+recoveryPassword = "recoveryPassword".encode('utf-8')
+
+app.setBTCVPassword(instantPassword, btchip.BTCV_PASSWORD_INSTANT)
+app.setBTCVPassword(recoveryPassword, btchip.BTCV_PASSWORD_RECOVERY)
+
+# Set password use
+instantPasswordHash = sha256(bytearray(instantPassword).hex().ljust(64, '0'))
+recoveryPasswordHash = sha256(bytearray(recoveryPassword).hex().ljust(64, '0'))
+
+app.setBTCVPasswordUse(bytearray(32), btchip.BTCV_TX_ALERT)
+app.setBTCVPasswordUse(instantPasswordHash, btchip.BTCV_TX_INSTANT)
+app.setBTCVPasswordUse(recoveryPasswordHash, btchip.BTCV_TX_RECOVERY)
+
+# Get new account
+address = eval(app.getWalletPublicKey("0'/0/0", btcvAddr=True)['address'])
+assert address.decode("utf-8")[0] == 'R'
