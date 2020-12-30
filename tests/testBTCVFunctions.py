4cdc89baba374a54d16d46c711ef9096ccd21e5d
Maciej Grychowski <maciej.grychowski@pragmaticcoders.com>
2020-12-30 16:05:41 +0100

    parameter for alert/instant/recovery pubkey derivation in getWalletPublicKey + test align after setBTCVPassword change

--- tests/testBTCVFunctions.py
+++ tests/testBTCVFunctions.py
@@ -16,25 +16,34 @@ app = btchip(dongle)
 
 # Set passwords
 # empty password
-app.setBTCVPassword(bytearray(1), btchip.BTCV_PASSWORD_INSTANT)
+app.setBTCVPassword("1", btchip.BTCV_PASSWORD_INSTANT)
 
-instantPassword = "instantPassword".encode('utf-8')
-recoveryPassword = "recoveryPassword".encode('utf-8')
+instantPassword = "instantPassword"
+recoveryPassword = "recoveryPassword"
 
 app.setBTCVPassword(instantPassword, btchip.BTCV_PASSWORD_INSTANT)
 app.setBTCVPassword(recoveryPassword, btchip.BTCV_PASSWORD_RECOVERY)
 
 # Set password use
-instantPasswordHash = sha256(bytearray(instantPassword).hex().ljust(64, '0'))
-recoveryPasswordHash = sha256(bytearray(recoveryPassword).hex().ljust(64, '0'))
+instantPasswordHash = sha256(bytearray(instantPassword.encode('utf-8')).hex().ljust(64, '0'))
+recoveryPasswordHash = sha256(bytearray(recoveryPassword.encode('utf-8')).hex().ljust(64, '0'))
 
 app.setBTCVPasswordUse(bytearray(32), btchip.BTCV_TX_ALERT)
 app.setBTCVPasswordUse(instantPasswordHash, btchip.BTCV_TX_INSTANT)
 app.setBTCVPasswordUse(recoveryPasswordHash, btchip.BTCV_TX_RECOVERY)
 
 # Get new account
-address = app.getWalletPublicKey("0'/0/0", btcvAddr=True)['address']
-assert address.decode("utf-8")[0] == 'R'
+result = app.getWalletPublicKey("0'/0/0", btcvAddr=True, btcvPubkeyTree=btchip.BTCV_TX_ALERT)
+address1 = result['address']
+pubkey1 = result['publicKey']
+assert address1.decode("utf-8")[0] == 'R'
+
+result = app.getWalletPublicKey("0'/0/0", btcvAddr=True, btcvPubkeyTree=btchip.BTCV_TX_INSTANT)
+address2 = result['address']
+pubkey2 = result['publicKey']
+assert address2.decode("utf-8")[0] == 'R'
+assert address2 == address1
+assert pubkey2 != pubkey1
 
 # Get new accounts
 addresses = app.getWalletPublicKeyBatch(["0'/0/0", "0'/0/1"], btcvAddr=True)
